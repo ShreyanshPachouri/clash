6b3fe4d177f85d20cf7b46debc2f5a2792c17bdf
Shreyansh Pachouri <pachourishreyansh@gmail.com>
2026-09-23 18:46:27 +0530

    Initial setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '../components/ui/button'
 
 export default function page() {
   return (
     <div>
       <h1 className="text-red-500">I am home page</h1>
+      <Button>Hello</Button>
     </div>
   )
 }
